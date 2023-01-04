df07278bdb1a9f6df6b2632f0b53d1d03f8ea4e3
Brian Fontillas <BrianKFontillas@gmail.com>
2023-01-03 16:20:46 -0800

    Card and deck objects building structure

---
+++ flow_notes.md
@@ -0,0 +1,87 @@
+# Notes
+
+## Card and deck object
+
+- The deck consists of 52 `cards`
+- Each single card is an individual Object
+    - What does this object look like?
+        -------
+        - Number
+        - Suit
+        -------
+- Since the deck consists of 52 cards,
+    - How can we structure the deck status with individual cards?
+    
+### Problem
+We need to create a structure that contains all individual (52) cards in a deck and store the value into a property (state)
+
+### example
+cons(number, suit)
+cards: [{}, {}...];
+[
+    {H: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K},
+    {D: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K},
+    {C: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K},
+    {S: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K}
+]
+
+new Card(3, C)
+
+{number: 3,
+ suit: C
+}
+
+[
+    {H: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K},
+    {D: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K},
+    {C: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K},
+    {S: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K}
+]
+
+{H: [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]}
+
+[A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]
+
+new Card(current number, current suit)
+{A, H}
+[] >> [{A, H}]
+{2, H}
+[{A, H}] >> [{A, H}, {2, H}] >>return
+....
+
+next
+
+{A, D}
+{2, D}
+
+....
+
+### Data structure
+Array with multiple objects (52)
+
+[
+    {},
+    {},
+    ...
+]
+
+### Algorithm
+
+Create a static method that displays an array of objects that contain the suit as the single key, and an array of all possible nums in that suite
+
+1. Create a static method
+2. Create an array with the objects
+3. the static method takes 1 argument but has a default of none
+    - the arg given will return the designated argument.
+        - This method only accepts strings of 'H', 'D', 'C', 'S' or no args
+
+
+main method
+
+[] empty array
+
+1. Iterate over each suit and its numbers
+2. for the current suit, iterate over the numbers array
+3. for each element within the array, create a new Card where the first arg is the number, and the second arg is the current object key name
+  - push the new card object into the empty array
+4. After iteration, return the "empty" array
--- twentyone.js
+++ twentyone.js
@@ -1,144 +1,153 @@
+"use strict";
+
+const readline = require('readline-sync');
+const SHUFFLE = require('shuffle-array');
+
 class Card {
-    constructor() {
-      //STUB
-      // What sort of state does a card need?
-      // Rank? Suit? Points?
+  constructor(number, suit) {
+    this.number = number;
+    this.suit = suit;
+  }
+}
+  
+class Deck {
+  static getDeck(suit) {
+    const deck = [
+      {"H": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]},
+      {"D": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]},
+      {"C": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]},
+      {"S": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]},
+    ];
+
+    switch (suit) {
+      case "H": return deck[0];
+      case "D": return deck[1];
+      case "C": return deck[2];
+      case "S": return deck[3];
+      default: return deck;
     }
   }
+
+  constructor() {
+    this.cards = null;
+  }
+
+  shuffle() {
+    this.cards = SHUFFLE(this.cards);
+  }
   
-  class Deck {
-    constructor() {
-      //STUB
-      // What sort of state does a deck need?
-      // 52 Cards?
-      // obviously, we need some data structure to keep track of cards
-      // array, object, something else?
-    }
-  
-    deal() {
+  deal() {
       //STUB
       // does the dealer or the deck deal?
-    }
   }
+}
   
-  class Participant {
-    constructor() {
-      //STUB
-      // What sort of state does a participant need?
-      // Score? Hand? Amount of money available?
-      // What else goes here? all the redundant behaviors from Player and Dealer?
-    }
+class Participant {
+  constructor() {
+    // this.cards = [];
   }
+
+  hit() {
+        //STUB
+  }
+    
+  stay() {
+    //STUB
+  }
+    
+  isBusted() {
+    //STUB
+  }
+    
+  score() {
+    //STUB
+  }
+}
   
-  class Player extends Participant {
-    constructor() {
+class Player extends Participant {
+  constructor() {
       //STUB
       // What sort of state does a player need?
       // Score? Hand? Amount of money available?
-    }
-  
-    hit() {
-      //STUB
-    }
-  
-    stay() {
-      //STUB
-    }
-  
-    isBusted() {
-      //STUB
-    }
-  
-    score() {
-      //STUB
-    }
   }
+}
   
-  class Dealer extends Participant {
+class Dealer extends Participant {
     // Very similar to a Player; do we need this?
   
-    constructor() {
+  constructor() {
       //STUB
       // What sort of state does a dealer need?
       // Score? Hand? Deck of cards? Bow tie?
-    }
-  
-    hit() {
-      //STUB
-    }
-  
-    stay() {
-      //STUB
-    }
-  
-    isBusted() {
-      //STUB
-    }
-  
-    score() {
-      //STUB
-    }
-  
-    hide() {
-      //STUB
-    }
-  
-    reveal() {
-      //STUB
-    }
-  
-    deal() {
-      //STUB
-      // does the dealer or the deck deal?
-    }
   }
   
-  class TwentyOneGame {
-    constructor() {
+  hide() {
       //STUB
-      // What sort of state does the game need?
-      // A deck? Two participants?
-    }
+  }
   
-    start() {
+  reveal() {
+      //STUB
+  }
+}
+  
+class TwentyOneGame {
+  constructor() {
+    this.deck = new Deck();
+  }
+  
+  initializeDeck() {
+    let allCards = [];
+
+    Deck.getDeck().forEach(suitGroup => {
+      let suit = Object.keys(suitGroup)[0];
+
+      suitGroup[suit].forEach(num => {
+        allCards.push(new Card(num, suit));
+      });
+    });
+
+    this.deck.cards = allCards;
+  }
+
+  start() {
       //SPIKE
-      this.displayWelcomeMessage();
-      this.dealCards();
-      this.showCards();
-      this.playerTurn();
-      this.dealerTurn();
-      this.displayResult();
-      this.displayGoodbyeMessage();
-    }
-  
-    dealCards() {
-      //STUB
-    }
-  
-    showCards() {
-      //STUB
-    }
-  
-    playerTurn() {
-      //STUB
-    }
-  
-    dealerTurn() {
-      //STUB
-    }
-  
-    displayWelcomeMessage() {
-      //STUB
-    }
-  
-    displayGoodbyeMessage() {
-      //STUB
-    }
-  
-    displayResult() {
-      //STUB
-    }
+    this.displayWelcomeMessage();
+    this.dealCards();
+    this.showCards();
+    this.playerTurn();
+    this.dealerTurn();
+    this.displayResult();
+    this.displayGoodbyeMessage();
   }
   
-  let game = new TwentyOneGame();
-  game.start();
+  dealCards() {
+      //STUB
+  }
+  
+  showCards() {
+      //STUB
+  }
+  
+  playerTurn() {
+      //STUB
+  }
+  
+  dealerTurn() {
+      //STUB
+  }
+  
+  displayWelcomeMessage() {
+      console.log("Welcome to Twenty-one!");
+  }
+  
+  displayGoodbyeMessage() {
+      console.log("Goodbye, thanks for playing!");
+  }
+  
+  displayResult() {
+      //STUB
+  }
+}
+  
+let game = new TwentyOneGame();
+game.start();
